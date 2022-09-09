6e0de823b768d70320ceeb793f936a1f3a2cf996
Hasan Daghash <hasandagash@gmail.com>
2022-09-10 01:31:34 +0400

    feat(component:list): add majils card

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,10 +4,31 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { FormsModule } from '@angular/forms';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule, NzButtonModule, NzGridModule, NzIconModule],
-  exports: [CommonModule, FormsModule, NzButtonModule, NzGridModule, NzIconModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    NzButtonModule,
+    NzGridModule,
+    NzIconModule,
+    NzCardModule,
+    NzTagModule,
+    NzToolTipModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    NzButtonModule,
+    NzGridModule,
+    NzIconModule,
+    NzCardModule,
+    NzTagModule,
+    NzToolTipModule
+  ]
 })
 export class SharedModule {}
