22cfe30791177a78af621fbaa4225cb2e6b343e7
Hasan Daghash <hasandagash@gmail.com>
2022-09-10 00:36:43 +0400

    feat(componenet:services): style service card

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { FormsModule } from '@angular/forms';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule, NzButtonModule],
-  exports: [CommonModule, FormsModule, NzButtonModule]
+  imports: [CommonModule, FormsModule, NzButtonModule, NzGridModule, NzIconModule],
+  exports: [CommonModule, FormsModule, NzButtonModule, NzGridModule, NzIconModule]
 })
 export class SharedModule {}
